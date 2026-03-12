ee948d384b8369ebd5e80bef2d1b74d39086bb13
safinartur603-coder <safinartur603@gmail.com>
2026-03-13 02:15:09 +0400

    Update: removed related events from reference

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2177155412" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "92828138" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
